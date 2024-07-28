0b48023f1b276103252068ca06b2b468fce2b35b
Sworaj2002 <162405000+Sworaj2002@users.noreply.github.com>
2024-07-28 20:32:08 +0530

    c

---
+++ home.py
@@ -0,0 +1,61 @@
+import streamlit as st
+def app():    
+    st.markdown(
+        '''
+        <h1 style='text-align:center;'>IPL MATCH PREDICTIONS AND ANALYSIS </h1>
+        <br>
+        <center>――――――――――――――――――――――――――――――――――――――――――――――――――――</center>
+        <center><img src='https://whiteandblack.in/wp-content/uploads/2021/04/IPL-teams-logo-design-breakdown-scaled.jpg' style="text-align:center; display:block; width:65%"></img> </center>
+
+        <div style='text-align:justified; display:flex; flex-direction:column; width:66%; margin-left:18%;'>
+
+        ――――――――――――――――――――――――――――――――――――――――――――――――――――
+        
+ 
+        ## **IPL** 🏏
+        IPL stands for Indian Premier League, a T20 cricket tournament which was originally established in 2008. It is traditionally played from April through to June each year.
+
+        The Indian Premier League starts with a round robin tournament where each franchise will play each other twice: home and away. Sides will earn two points for a win while the teams will earn one point each if there is an abandoned game.
+
+        
+        At the end of this sequence, the top four sides in the table will progress to the playoffs while the remaining franchises will be eliminated.The playoffs start with the first qualifier where the top two sides in the table play each other and the winner goes straight through to the final.
+
+
+        Next up is the eliminator where the third and fourth placed sides face off. The winner goes through to the second qualifier while the loser is eliminated. In the second qualifier, the winner of the eliminator takes on the loser of the first playoff match. The winner of that game progresses to the final.
+        
+        <br>
+        ――――――――――――――――――――――――――――――――――――――――――――――――――――
+        <br>
+
+        ## **Teams** 👨‍💼
+        * Chennai Super Kings
+        * Royal Challengers Bangalore
+        * Delhi Capitals
+        * Punjab Kings
+        * Sunrisers Hyderabad
+        * Mumbai Indians
+        * Rajasthan Royals
+        * Kolkata Knight Riders
+        
+        <br>
+        
+        ――――――――――――――――――――――――――――――――――――――――――――――――――――
+        
+        <br>
+        
+        ## You Are Interacting With A **Multipurpose Web App** That Can- 
+        
+        ###### 1. Predict The First Inning Score Based On Current Situation of The Match.
+        ###### 2. Provide A Winner Probability For Any Condition of Second Innings of the Match.
+        ###### 3. Detailed Exploratory Data Analysis On Dataset.
+        ###### 4. Team vs Team Analysis.
+        ###### 5. Player vs Player Analysis.
+        ###### 6. Team Past Record Analysis.
+        ###### 7. Player Career Analysis As Batsman and Bowler.
+        
+        <br>
+        
+        ――――――――――――――――――――――――――――――――――――――――――――――――――――
+        </div>
+        ''',unsafe_allow_html=True
+    )
